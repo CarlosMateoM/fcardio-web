7652a820f01c1c263113bbed9e62018b7ad2c92a
CarlosMateoM <carlosmateo484@gmail.com>
2024-12-03 15:53:07 -0500

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('../views/auth/LoginView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/auth/RegisterView.vue'),
+    },
+  ],
+})
+
+export default router
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+<main class="flex w-full  border min-h-screen justify-center items-center"  >
+
+    <form class="bg-white p-4 rounded-md " action="">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Login</button>
+    </form>
+</main>
+</template>
+<script setup>
+</script>
